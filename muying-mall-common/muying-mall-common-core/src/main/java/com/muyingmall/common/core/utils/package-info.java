459cd9135518950388fa6e44cb7003ec1601fef9
vam12375 <2898191344@qq.com>
2025-09-23 19:34:42 +0800

    docs(core): 更新utils包文档
    - 更新package-info.java文档，添加新迁移工具类的说明
    - 提供完整的使用示例和注意事项
    - 包含所有工具类的功能描述和API链接
    
    提交人：青柠檬
    提交时间：2025-09-23 19:34:42

--- muying-mall-common/muying-mall-common-core/src/main/java/com/muyingmall/common/core/utils/package-info.java
+++ muying-mall-common/muying-mall-common-core/src/main/java/com/muyingmall/common/core/utils/package-info.java
@@ -1,10 +1,45 @@
 /**
  * 通用工具类包
  * 
- * <p>包含系统中使用的各种通用工具类</p>
+ * <p>提供各种通用的工具类，包括：</p>
+ * <ul>
+ *   <li>{@link com.muyingmall.common.core.utils.JwtUtils} - JWT令牌工具类</li>
+ *   <li>{@link com.muyingmall.common.core.utils.RedisUtils} - Redis缓存工具类</li>
+ *   <li>{@link com.muyingmall.common.core.utils.EnumUtils} - 枚举工具类</li>
+ *   <li>{@link com.muyingmall.common.core.utils.SpringContextUtils} - Spring上下文工具类</li>
+ *   <li>{@link com.muyingmall.common.core.utils.SecurityUtils} - 安全工具类</li>
+ * </ul>
  * 
- * @author MuyingMall Team
- * @version 1.0.0
+ * <p>使用示例：</p>
+ * <pre>{@code
+ * // JWT操作
+ * String token = jwtUtils.generateToken(1001, "admin", "ADMIN");
+ * boolean isValid = jwtUtils.validateToken(token);
+ * 
+ * // Redis操作
+ * redisUtils.set("key", "value", 3600);
+ * Object value = redisUtils.get("key");
+ * 
+ * // 枚举操作
+ * OrderStatus status = EnumUtils.getOrderStatusByCode("PAID");
+ * 
+ * // Spring Bean获取
+ * UserService userService = SpringContextUtils.getBean(UserService.class);
+ * 
+ * // 安全操作
+ * String hashedPassword = SecurityUtils.hashPassword("password", salt);
+ * }</pre>
+ * 
+ * <p>注意事项：</p>
+ * <ul>
+ *   <li>所有工具类都是无状态的，可以安全地在多线程环境中使用</li>
+ *   <li>部分工具类依赖Spring环境，使用前请确保相关依赖已配置</li>
+ *   <li>Redis相关工具类需要Redis服务可用</li>
+ *   <li>JWT工具类需要配置密钥和过期时间</li>
+ * </ul>
+ * 
+ * @author 母婴商城开发团队
  * @since 2025-09-23
+ * @version 1.0
  */
 package com.muyingmall.common.core.utils;
